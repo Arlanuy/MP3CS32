a0de14465ec1522f6d57ce59ec6aacf523ccb191
Arlan Vincent Uy <arlanvincentuy@gmail.com>
2017-04-20 09:37:57 +0000

    cost adj mat almost finish

--- for_more_shopping.c
+++ for_more_shopping.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#define FALSE 0
+#define TRUE 1
 
 char* printMenuWithDataIter(char* menu_choice, int menuchoice_size) {
     printf("How would you like your input to be further processed?\n");
@@ -23,7 +27,7 @@ int main(void) {
     printf("Enter the name of the input file: ");
     fgets(input_name, filename_size, stdin);
     input_name[strlen(input_name) - 1] = '\0';
-    input_file = fopen(input_name, "a");
+    input_file = fopen(input_name, "r");
     assert(input_file != NULL);
     
     //for output
@@ -40,13 +44,32 @@ int main(void) {
     char* line = malloc(sizeof(line_size)), *line2 = malloc(sizeof(line_size));
     char* menu_choice = malloc(sizeof(char) * menuchoice_size);
     assert(menu_choice != NULL);
-    int more_dataset = TRUE;
-    while (more_dataset == TRUE && fgets(line, LINELENGTH, input_file)) {
-        line[strlen(line) - 1] = '\0';
-        
-        
+    int more_dataset = TRUE, line_iter, start_line_iter, space_iter, row_iter, line_bounds;
+    int** cost_adj_mat;
+    while (more_dataset == TRUE) {
+        row_iter = 0;
+        while (fgets(line, line_size, input_file)) {
+            space_iter = 0;
+            start_line_iter = 0;
+            line[strlen(line) - 1] = '\0';
+            cost_adj_mat = realloc(cost_adj_mat, (row_iter + 1) * sizeof(int*));
+            printf("string line is %s\n", line);
+            line_bounds = strlen(line);
+            for (line_iter = 0; line_iter < line_bounds; line_iter++) {
+                if (line[line_iter] == ' ') {
+                    line[line_iter] = '\0';
+                    cost_adj_mat[row_iter] = realloc(cost_adj_mat[row_iter], (space_iter + 1) * sizeof(int));
+                    cost_adj_mat[row_iter][space_iter] = atoi(&line[start_line_iter]);
+                    printf("string is %s at i %d and j %d is %d\n", &line[start_line_iter], row_iter, space_iter, cost_adj_mat[row_iter][space_iter]);
+                    space_iter++;
+                    start_line_iter = line_iter + 1;
+                }
+            }
+            
+            row_iter++;  
+        }
         //another choice of input mode
-        
+
         success_choice = FALSE;
         while (success_choice == FALSE) {
             menu_choice = printMenuWithDataIter(menu_choice, menuchoice_size);
@@ -71,6 +94,6 @@ int main(void) {
         }
      }
     
-    printf("Enter the vertex pairs needed on the termal line\n");
+    printf("Enter the vertex pairs needed on the termal line (separate it by space)\n");
     printf("Enter 2 for processing through an input file\n");
 }
