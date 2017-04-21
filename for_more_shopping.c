8522a4092504d23d713f68a508258ff2589b5677
Arlan Vincent Uy <arlanvincentuy@gmail.com>
2017-04-21 20:21:33 +0000

    finish file and vertex system

--- for_more_shopping.c
+++ for_more_shopping.c
@@ -16,6 +16,32 @@ char* printMenuWithDataIter(char* menu_choice, int menuchoice_size) {
     return menu_choice;
 }
 
+int getNumOfVertex(char* cost_row) {
+    int i, vertex_num = 1;
+    for (i = 0; cost_row[i] != '\n'; i++) {
+        if (cost_row[i] == ' ') {
+            vertex_num++;
+        }
+    }
+    printf("vertex num is %d\n", vertex_num);
+    return vertex_num;
+}
+
+void freeStrCostAdjMat(char** str_cost_adj_mat, int num_of_vertex) {
+    int i;
+    for (i = 0; i <  num_of_vertex; i++) {
+        free(str_cost_adj_mat[i]);
+    }
+}
+
+void freeCostAdjMat(int** cost_adj_mat, int num_of_vertex) {
+    int i;
+    for (i = 0; i <  num_of_vertex; i++) {
+        free(cost_adj_mat[i]);
+    }
+    
+}
+
 int main(void) {
      int filename_size = 50, line_size = 500, string_size = 70, menuchoice_size = 10;
 
@@ -24,56 +50,81 @@ int main(void) {
     input_name = malloc(sizeof(char) * filename_size);
     assert(input_name != NULL);
     FILE* input_file, *output_file;
+    /**
     printf("Enter the name of the input file: ");
     fgets(input_name, filename_size, stdin);
     input_name[strlen(input_name) - 1] = '\0';
     input_file = fopen(input_name, "r");
-    assert(input_file != NULL);
+    assert(input_file != NULL); */
+    input_file = fopen("input.txt", "r");
     
     //for output
+    /**
     char* output_name = malloc(sizeof(char) * filename_size);
     assert(output_name != NULL);
     printf("Enter the name of the output file: ");
     fgets(output_name, filename_size, stdin);
     output_name[strlen(output_name) - 1] = '\0';
     output_file = fopen(output_name, "a");
-    assert(output_file != NULL);
+    assert(output_file != NULL); */
+    
+    output_file = fopen("output.txt", "a");
     
     //for menu choice
     int success_choice = FALSE, num_menu;
-    char* line = malloc(sizeof(line_size)), *line2 = malloc(sizeof(line_size));
+    char* line = malloc(sizeof(line_size)), *str_vertex_pair = malloc(sizeof(line_size));
     char* menu_choice = malloc(sizeof(char) * menuchoice_size);
     assert(menu_choice != NULL);
-    int more_dataset = TRUE, line_iter, start_line_iter, space_iter, row_iter, line_bounds;
-    int** cost_adj_mat;
+    int more_dataset = TRUE, line_iter, start_line_iter, space_iter, row_iter = 0, line_bounds;
+    int** cost_adj_mat = NULL;
+    char** str_cost_adj_mat = NULL;
+    int num_of_vertex;//, str_cost_alloc_iter, cost_alloc_iter;
     while (more_dataset == TRUE) {
-        row_iter = 0;
-        while (fgets(line, line_size, input_file)) {
-            space_iter = 0;
-            start_line_iter = 0;
-            line[strlen(line) - 1] = '\0';
-            cost_adj_mat = realloc(cost_adj_mat, (row_iter + 1) * sizeof(int*));
-            printf("string line is %s\n", line);
-            line_bounds = strlen(line);
-            for (line_iter = 0; line_iter < line_bounds; line_iter++) {
-                if (line[line_iter] == ' ') {
-                    line[line_iter] = '\0';
-                    cost_adj_mat[row_iter] = realloc(cost_adj_mat[row_iter], (space_iter + 1) * sizeof(int));
-                    cost_adj_mat[row_iter][space_iter] = atoi(&line[start_line_iter]);
-                    printf("string is %s at i %d and j %d is %d\n", &line[start_line_iter], row_iter, space_iter, cost_adj_mat[row_iter][space_iter]);
-                    space_iter++;
-                    start_line_iter = line_iter + 1;
+        menu_choice = printMenuWithDataIter(menu_choice, menuchoice_size);
+        switch(menu_choice[0] - '0') {
+            case 0:
+                row_iter = 0;
+                while (fgets(line, line_size, input_file) && strcmp(line, "end\n") != 0) {
+                    if (row_iter == 0) {
+                        num_of_vertex = getNumOfVertex(line);
+                        str_cost_adj_mat = realloc(str_cost_adj_mat, num_of_vertex * sizeof(char*));
+                        cost_adj_mat = realloc(cost_adj_mat, num_of_vertex * sizeof(int*));
+                    }
+                    space_iter = 0;
+                    start_line_iter = 0;
+                    //cost_adj_mat = realloc(cost_adj_mat, (row_iter + 1) * sizeof(int*));
+                    printf("string line is %s\n", line);
+                    line_bounds = strlen(line);
+                    for (line_iter = 0; line_iter < line_bounds; line_iter++) {
+                        if (line[line_iter] == ' ' || line[line_iter] == '\n') {
+                            line[line_iter] = '\0';
+                            cost_adj_mat[row_iter] = realloc(cost_adj_mat[row_iter], (space_iter + 1) * sizeof(int));
+                            cost_adj_mat[row_iter][space_iter] = atoi(&line[start_line_iter]);
+                            printf("string is %s at i %d and j %d is %d\n", &line[start_line_iter], row_iter, space_iter, cost_adj_mat[row_iter][space_iter]);
+                            space_iter++;
+                            start_line_iter = line_iter + 1;
+                        }
+                    }
+                    line[line_bounds - 1] = '\0';
+                    str_cost_adj_mat[row_iter] = realloc(str_cost_adj_mat[row_iter], (num_of_vertex * 5 * sizeof(char)) + 4); //5 for the amount of characters given a 3 digit number and a space, + 4 for extra characters like '\n' and carriage return
+                    str_cost_adj_mat[row_iter] = line;
+                    row_iter++;  
                 }
-            }
+                
+            case 1:
+                printf("Enter a vertex pair (separate it by space): ");          
+                fgets(str_vertex_pair, string_size, stdin);
+                str_vertex_pair[strlen(str_vertex_pair) - 1] = '\0'; 
+                break;
             
-            row_iter++;  
         }
+        
         //another choice of input mode
 
         success_choice = FALSE;
         while (success_choice == FALSE) {
             menu_choice = printMenuWithDataIter(menu_choice, menuchoice_size);
-            printf("%s\n", menu_choice);
+            printf("mc %s\n", menu_choice);
             if (strlen(menu_choice) == 1) {
                 num_menu = menu_choice[0] - '0';
                 if ((num_menu >= 0) && (num_menu <= 2)) {
@@ -89,11 +140,16 @@ int main(void) {
                 printf("Enter a choice of length 1 only");
             }
         }
-        if (num_menu != 0) {
+        if (num_menu == 2) {
             more_dataset = FALSE;
         }
      }
-    
-    printf("Enter the vertex pairs needed on the termal line (separate it by space)\n");
-    printf("Enter 2 for processing through an input file\n");
+     freeStrCostAdjMat(str_cost_adj_mat, num_of_vertex);
+     free(str_cost_adj_mat);
+     freeCostAdjMat(cost_adj_mat, num_of_vertex);
+     free(cost_adj_mat);
+     free(input_name);
+     free(line);
+     free(str_vertex_pair);
+     free(menu_choice);
 }
